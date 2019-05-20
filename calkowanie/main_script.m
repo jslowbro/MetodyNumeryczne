f331c9af944381452976665d4b8cff996d4b4db9
jslowbro <38038199+jslowbro@users.noreply.github.com>
2019-05-20 19:43:37 +0200

    major bug fix in newton_cotes_vc

--- calkowanie/main_script.m
+++ calkowanie/main_script.m
@@ -1,4 +1,11 @@
 f = @(x) 11*x^10;
-fv = f_values_vector(-1,1,0.01, f)
-newton_cotes([0, 0.0107, 11], 0.5)
-adv_newton_cotes(fv, 1, 0.01)
+fv = f_values_vector(-1,1,0.01, f);
+adv_newton_cotes(fv, 2, 0.01)
+%newton_cotes([0, 0.0107, 11], 0.5);
+x = newton_cotes_vc(fv,-1, 1, 100, 2)
+
+
+loglog([1:1000], abs(result_nc_1 – 2.0),...
+  [1:1000], abs(result_nc_2 – 2.0),...
+  [1:1000], abs(result_nc_3 – 2.0),...
+  [1:1000], abs(result_nc_4 - 2.0)) 
--- calkowanie/newton_cotes_vc.m
+++ calkowanie/newton_cotes_vc.m
@@ -1,5 +1,5 @@
 function nc_vec = newton_cotes_vc(fv,a,b,iter, d)
 nc_vec = zeros(1, iter);
 for i=1: iter
-    nc_vec(i) = adv_newton_cotes(fv,(b-a)/d*i);
+    nc_vec(i) = adv_newton_cotes(fv,d,(b-a)/(d*i));
 end 
